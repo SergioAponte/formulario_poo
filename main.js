89fe2977219f1884318268cd9d37d6fadbbf914b
marianita13 <marianitacero.1913@gmail.com>
2023-03-17 13:54:01 -0500

    mejora

--- main.js
+++ main.js
@@ -11,8 +11,20 @@ class Persona{
         this.fecha_nacimiento=fecha_nacimiento
         this.edad=edad
     }
-    edad(){
-        
+
+    nombre_completo(){
+        return `${this.nombre} ${this.apellido}`
+    }
+    años(){
+        let datos_años=this.fecha_nacimiento.split('-')
+        año=datos_años[0]
+        meses=datos_años[1]
+        dia=datos_años[2]
+        if (parseInt(fechas.getMonth()+1 === meses && parseInt(fechas.getDay() === dia))){
+            return true
+        }else{
+            return false
+        }
     }
 }
 
@@ -21,21 +33,54 @@ let nombre=document.getElementById('nombre')
 let apellido=document.getElementById('apellido')
 let fecha=document.getElementById('fecha')
 let cedula=document.getElementById('cedula')
-const formulario=document.getElementById('guardar')
-let fechas= new Date();
-console.log(fechas)
+let nombre_apellido= document.getElementById('nombre_apellido')
 
-// console.log(fechas)
+let pregunta=document.getElementById('pregunta')
+let mensajes=document.getElementById('mensaje')
+const formulario=document.getElementById('guardar')
+let fechas= new Date
+
 formulario.addEventListener('click',event=>{
     if(event.target.className=='guardar'){
-        if(nombre==''|| apellido=='' || fecha=='' || cedula==''){
+        if(nombre.value==''|| apellido.value=='' || fecha.value=='' || cedula.value==''){
             alert('falta compĺetar un espacio')
         }
-        const info= new Persona(nombre.value,apellido.value,(fechas.value-fecha.value),cedula.value)
+
+        datos_años=fechas.value.split('-')
+        año=datos_años[0]
+        meses=datos_años[1]
+        
+        console.log(datos_años)
+
+        const info= new Persona(cedula.value,nombre.value,apellido.value,())
         personas.push(info)
         console.log(personas)
+        console.log(info.nombre_completo())
+
+        const informacion=document.createElement('p')
+        const pregunta2=document.createElement('p')
+        const mensaje=document.createElement('p')
+
+        nombre_apellido.append(informacion)
+        pregunta.append(pregunta2)
+        mensajes.append(mensaje)
+
+
+        informacion.innerText=info.nombre_completo()
+        
+
+        if(años()){
+            mensaje.innerText='FELIZ CUMPLEAÑ OS'
+        }else{
+            mensaje.innerText=''
+        }
 
     }
+
     
 })
-// console.log(informacion)
+
+
+
+
+
